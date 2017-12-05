0b223dd37591934d7b5486f550029dd7b679d765
Mitchell Goshorn <msgoshorn@gmail.com>
2017-12-04 20:30:30 -0500

    Basic outline of User bean and some basic DAO support

--- src/main/java/com/acebook/dao/UserDaoHibernate.java
+++ src/main/java/com/acebook/dao/UserDaoHibernate.java
@@ -1,5 +1,8 @@
 package com.acebook.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -39,7 +42,7 @@ public class UserDaoHibernate implements UserDao {
 		se.close();
 		return user;
 	}
-
+	
 	@Override
 	public User load(final int id) {
 		Session se = su.getSession();
@@ -68,5 +71,41 @@ public class UserDaoHibernate implements UserDao {
 		User user = (User) c.uniqueResult();
 		return user;
 	}
+
+	@Override
+	public User update(User user) {
+		Session se = su.getSession();
+		Transaction tx = se.beginTransaction();
+		se.update(user);
+		tx.commit();
+		se.close();
+		return user;
+	}
+
+	@Override
+	public User merge(User user) {
+		Session se = su.getSession();
+		Transaction tx = se.beginTransaction();
+		se.merge(user);
+		tx.commit();
+		se.close();
+		return user;
+	}
+
+	@Override
+	public List<User> getFriends(User user) {
+		// TODO Relies on implementation of friend join table
+		throw new UnsupportedOperationException("Not implemented yet");
+	}
+
+	@Override
+	public Optional<User> getUserByUsername(String username) {
+		Session se = su.getSession();
+		Criteria c = se.createCriteria(User.class);
+		c.add(Restrictions.eq("username", username));
+		return Optional.ofNullable((User) c.uniqueResult());
+	}
+	
+	
 
 }
--- src/main/java/com/acebook/entities/User.java
+++ src/main/java/com/acebook/entities/User.java
@@ -3,10 +3,20 @@ package com.acebook.entities;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
+/**
+ * Entity representing an Acebook user.
+ *  
+ * @author Mitchell Goshorn
+ *
+ */
 @Entity
 public class User {
 
+	@Id
+	// @SequenceGenerator TODO
+	// @GeneratedValue TODO
 	private int user_id;
 	private String username;
 	private String email;
@@ -15,7 +25,154 @@ public class User {
 	private LocalDate birthdate;
 	private String hash;
 	private String salt;
-	
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDate getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((birthdate == null) ? 0 : birthdate.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((hash == null) ? 0 : hash.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((salt == null) ? 0 : salt.hashCode());
+		result = prime * result + user_id;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (birthdate == null) {
+			if (other.birthdate != null)
+				return false;
+		} else if (!birthdate.equals(other.birthdate))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (hash == null) {
+			if (other.hash != null)
+				return false;
+		} else if (!hash.equals(other.hash))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (salt == null) {
+			if (other.salt != null)
+				return false;
+		} else if (!salt.equals(other.salt))
+			return false;
+		if (user_id != other.user_id)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [user_id=" + user_id + ", username=" + username + ", email=" + email + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", birthdate=" + birthdate + ", hash=" + hash + ", salt=" + salt + "]";
+	}
+
+	public User(int user_id, String username, String email, String firstName, String lastName, LocalDate birthdate,
+			String hash, String salt) {
+		super();
+		this.user_id = user_id;
+		this.username = username;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthdate = birthdate;
+		this.hash = hash;
+		this.salt = salt;
+	}
+
 	public User() {
 		// TODO Auto-generated constructor stub
 	}
