980ca4b8815705c15b654be91e1d022bbcebad95
Mitchell Goshorn <msgoshorn@gmail.com>
2017-12-11 12:37:36 -0500

    Misc

---
+++ AceBook/src/app/components/wall/wall.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-wall',
+  templateUrl: './wall.component.html',
+  styleUrls: ['./wall.component.css']
+})
+export class WallComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
